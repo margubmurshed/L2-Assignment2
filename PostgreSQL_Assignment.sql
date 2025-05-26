4963cfef9bf2815ad0ca40f102b0d0a23c46ca02
Margub Murshed <murshedmargub@gmail.com>
2025-05-26 12:21:09 +0600

    table creation queries updated

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,3 +1,26 @@
+create table rangers(
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    region VARCHAR(50) NOT NULL
+)
+
+create table species(
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(50) NOT NULL,
+    scientific_name TEXT NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status TEXT NOT NULL
+)
+
+create table sightings(
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INTEGER REFERENCES rangers(ranger_id),
+    species_id INTEGER REFERENCES species(species_id),
+    sighting_time TIMESTAMP NOT NULL,
+    location TEXT NOT NULL,
+    notes TEXT
+)
+
 -- 1. Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
 INSERT INTO rangers(name, region) VALUES ('Derek Fox', 'Coastal Plains');
 
